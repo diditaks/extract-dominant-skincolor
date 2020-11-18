c7beb4a95e09f1cbf022896399431c23ecabf139
diditaks <73362669+diditaks@users.noreply.github.com>
2020-11-18 13:49:27 +0100

    Update extract_skincolor.py

--- extract_skincolor.py
+++ extract_skincolor.py
@@ -181,8 +181,7 @@ def main():
     
 	if choice== 'About Me':
          st.subheader("Welcome")
-         imge = cv2.imread('makeup.png')
-		cv2.imshow(imge)	
+         imge = Image.open('makeup.png')
 
          st.write("""Hello, my name is Adel Takawira and I am fifteen years old. I attend the international school of the hague, and for my personal project, I created a color detecting software. Coming up with this idea required a lot of brainstorming on my part. But after combining my love for skincare and technology, I came up with this idea.""")
          basewidth = 700
@@ -197,8 +196,7 @@ def main():
         """)
 
 	if choice== 'Detection':
-         imagg = cv2.imread('FSnm.jpg')
-	 cv2.imshow(imagg)
+         imagg = Image.open('/Users/adeltakawira/Desktop/FSnm.jpg')
          basewidth = 700
          wpercent = (basewidth/float(imagg.size[0]))
          hsize = int((float(imagg.size[1])*float(wpercent)))
