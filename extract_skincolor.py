f3bed056487b8abbff032ef33d7c895049975215
diditaks <73362669+diditaks@users.noreply.github.com>
2020-11-19 20:42:14 +0100

    Update extract_skincolor.py

--- extract_skincolor.py
+++ extract_skincolor.py
@@ -181,7 +181,14 @@ def main():
     
 	if choice== 'About Me':
          st.subheader("Welcome")
-	
+         imge = Image.open('./FSnm.jpg',)
+
+         st.write("""Hello, my name is Adel Takawira and I am fifteen years old. I attend the international school of the hague, and for my personal project, I created a color detecting software. Coming up with this idea required a lot of brainstorming on my part. But after combining my love for skincare and technology, I came up with this idea.""")
+         basewidth = 700
+         wpercent = (basewidth/float(imge.size[0]))
+         hsize = int((float(imge.size[1])*float(wpercent)))
+         imge = imge.resize((basewidth,hsize), Image.ANTIALIAS)
+         st.image(imge)
          st.write("""Hello, my name is Adel Takawira and I am fifteen years old. I attend the international school of the hague, and for my personal project, I created a color detecting software. Coming up with this idea required a lot of brainstorming on my part. But after combining my love for skincare and technology, I came up with this idea.""")
          st.write("""Hello, my name is Adel Takawira and I am fifteen years old. I attend the international school of the hague, and for my personal project, I created a color detecting software. Coming up with this idea required a lot of brainstorming on my part. But after combining my love for skincare and technology, I came up with this idea.""")
          st.write("""Many people struggle to find a foundation or concealer that is an exact match to their skin tone, especially dark skinned people. For a long time they would have to resort to mixing two different shades in order get a color that was somewhat close to their natural tone. This is where I had the idea to create this software that detects, and extracts a person’s most dominant skin color, then displays it in a colorbar. This information can be used by cosmetic companies, to create a customized foundation/concealer that is suitable for their customers unique tone of skin. 
