1ef6524295c912c9720df30737c77b06d4f076d4
diditaks <73362669+diditaks@users.noreply.github.com>
2020-11-27 08:52:14 +0100

    Update extract_skincolor.py

--- extract_skincolor.py
+++ extract_skincolor.py
@@ -5,27 +5,29 @@ import numpy as np
 import cv2
 from sklearn.cluster import KMeans
 from collections import Counter
+import imutils
 import pprint
 from matplotlib import pyplot as plt
 import streamlit as st
 from PIL import Image
 
-st.write("""
-# Find Your Dominant Skincolor 
 
-Have you struggled to find the right foundation and concealer because you don't know the dominant tones of your skin? Or perhaps, you weren't able to find the right color tights to go with your unique skin tone?
+st.write("""
+# Find Your Dominant Skincolour 
+
+Have you struggled to find the right foundation and concealer because you don't know your exact skintone? Or perhaps, you weren't able to find the right colour tights to go with your unique skin tone?
 
 """)
 
     
-def extractSkin(image):
+def detectSkin(image):
     
     img = image.copy()
     
     # Converting from BGR Colours Space to HSV
     img = np.array(img)
     
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColour(img, cv2.COLOR_BGR2HSV)
 
     # Defining HSV Threadholds
     lower = np.array([0, 48, 80], dtype=np.uint8)
@@ -42,7 +44,7 @@ def extractSkin(image):
     skin = cv2.bitwise_and(img, img, mask=skinMask)
 
     # Return the Skin image
-    return cv2.cvtColor(skin, cv2.COLOR_HSV2BGR)
+    return cv2.cvtColour(skin, cv2.COLOR_HSV2BGR)
 
 
 def removeBlack(estimator_labels, estimator_cluster):
@@ -50,20 +52,20 @@ def removeBlack(estimator_labels, estimator_cluster):
     # Check for black
     hasBlack = False
 
-    # Get the total number of occurance for each color
+    # Get the total number of occurance for each colour
     occurance_counter = Counter(estimator_labels)
 
     # Quick lambda function to compare to lists
     def compare(x, y): return Counter(x) == Counter(y)
 
-    # Loop through the most common occuring color
+    # Loop through the most common occuring colour
     for x in occurance_counter.most_common(len(estimator_cluster)):
 
         # Quick List comprehension to convert each of RBG Numbers to int
-        color = [int(i) for i in estimator_cluster[x[0]].tolist()]
+        colour = [int(i) for i in estimator_cluster[x[0]].tolist()]
 
-        # Check if the color is [0,0,0] that if it is black
-        if compare(color, [0, 0, 0]) == True:
+        # Check if the colour is [0,0,0] that if it is black
+        if compare(colour, [0, 0, 0]) == True:
             # delete the occurance
             del occurance_counter[x[0]]
             # remove the cluster
@@ -74,13 +76,13 @@ def removeBlack(estimator_labels, estimator_cluster):
     return (occurance_counter, estimator_cluster, hasBlack)
 
 
-def getColorInformation(estimator_labels, estimator_cluster, hasThresholding=False):
+def getColourData(estimator_labels, estimator_cluster, hasThresholding=False):
 
-    # Variable to keep count of the occurance of each color predicted
+    # Variable to keep count of the occurance of each colour predicted
     occurance_counter = None
 
     # Output list variable to return
-    colorInformation = []
+    colourInformation = []
 
     # Check for Black
     hasBlack = False
@@ -100,7 +102,7 @@ def getColorInformation(estimator_labels, estimator_cluster, hasThresholding=Fal
     # Get the total sum of all the predicted occurances
     totalOccurance = sum(occurance_counter.values())
 
-    # Loop through all the predicted colors
+    # Loop through all the predicted colours
     for x in occurance_counter.most_common(len(estimator_cluster)):
 
         index = (int(x[0]))
@@ -109,67 +111,67 @@ def getColorInformation(estimator_labels, estimator_cluster, hasThresholding=Fal
         index = (index-1) if ((hasThresholding & hasBlack)
                               & (int(index) != 0)) else index
 
-        # Get the color number into a list
-        color = estimator_cluster[index].tolist()
+        # Get the colour number into a list
+        colour = estimator_cluster[index].tolist()
 
-        # Get the percentage of each color
-        color_percentage = (x[1]/totalOccurance)
+        # Get the percentage of each colour
+        colour_percentage = (x[1]/totalOccurance)
 
         # make the dictionay of the information
-        colorInfo = {"cluster_index": index, "color": color,
-                     "color_percentage": color_percentage}
+        colourInfo = {"cluster_index": index, "colour": colour,
+                     "colour_percentage": colour_percentage}
 
         # Add the dictionary to the list
-        colorInformation.append(colorInfo)
+        colourInformation.append(colourInfo)
 
-    return colorInformation
+    return colourInformation
 
 
-def extractDominantColor(image, number_of_colors=3, hasThresholding=False):
+def extractDominantTone(image, number_of_colours=3, hasThresholding=False):
 
     # Quick Fix Increase cluster counter to neglect the black(Read Article)
     if hasThresholding == True:
-        number_of_colors += 1
+        number_of_colours += 1
 
     # Taking Copy of the image
     img = image.copy()
 
     # Convert Image into RGB Colours Space
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColour(img, cv2.COLOR_BGR2RGB)
 
     # Reshape Image
     img = img.reshape((img.shape[0]*img.shape[1]), 3)
 
     # Initiate KMeans Object
-    estimator = KMeans(n_clusters=number_of_colors, random_state=0)
+    estimator = KMeans(n_clusters=number_of_colours, random_state=0)
 
     # Fit the image
     estimator.fit(img)
 
     # Get Colour Information
-    colorInformation = getColorInformation(
+    colourInformation = getColourData(
         estimator.labels_, estimator.cluster_centers_, hasThresholding)
-    return colorInformation
+    return colourInformation
 
 
-def plotColorBar(colorInformation):
+def graphColourBar(colourInformation):
     # Create a 500x100 black image
-    color_bar = np.zeros((100, 500, 3), dtype="uint8")
+    colour_bar = np.zeros((100, 500, 3), dtype="uint8")
 
     top_x = 0
-    for x in colorInformation:
-        bottom_x = top_x + (x["color_percentage"] * color_bar.shape[1])
+    for x in colourInformation:
+        bottom_x = top_x + (x["colour_percentage"] * colour_bar.shape[1])
 
-        color = tuple(map(int, (x['color'])))
+        colour = tuple(map(int, (x['colour'])))
 
-        cv2.rectangle(color_bar, (int(top_x), 0),
-                      (int(bottom_x), color_bar.shape[0]), color, -1)
+        cv2.rectangle(colour_bar, (int(top_x), 0),
+                      (int(bottom_x), colour_bar.shape[0]), colour, -1)
         top_x = bottom_x
-    return color_bar
+    return colour_bar
     
     
-def pretty_print_data(color_info):
-        for x in color_info:
+def pretty_print_data(colour_info):
+        for x in colour_info:
             print(pprint.pformat(x))
             print()
         
@@ -181,15 +183,28 @@ def main():
     
 	if choice== 'About Me':
          st.subheader("Welcome")
-         st.write("""Hello, my name is Adel, and I am currently doing MYP. For my personal project, I decided to create a color detecting software. Coming up with this idea required a lot of brainstorming on my part. But after combining my love for skincare and technology, I came up with this idea.""")
-         st.write("""Many people struggle to find a foundation or concealer that is an exact match to their skin tone, especially dark skinned people. For a long time they would have to resort to mixing two different shades in order get a color that was somewhat close to their natural tone. This is where I had the idea to create this software that detects, and extracts a person’s most dominant skin color, then displays it in a colorbar. This information can be used by cosmetic companies, to create a customized foundation/concealer that is suitable for their customers unique tone of skin. 
-        The great thing about this software is that it can be used, not only for cosmetic purposes, but also to customize ‘skin colored’ tights.""")
+         imge = Image.open('/Users/adeltakawira/Desktop/makeup.png',)
+
+         st.write("""Hello, my name is Adel Takawira and I am fifteen years old. I attend the international school of the hague, and for my personal project, I created a colour detecting software. Coming up with this idea required a lot of brainstorming on my part. But after combining my love for skincare and technology, I came up with this idea.""")
+         basewidth = 700
+         wpercent = (basewidth/float(imge.size[0]))
+         hsize = int((float(imge.size[1])*float(wpercent)))
+         imge = imge.resize((basewidth,hsize), Image.ANTIALIAS)
+         st.image(imge)
+         st.write("""Many people struggle to find a foundation or concealer that is an exact match to their skin tone, especially dark skinned people. For a long time they would have to resort to mixing two different shades in order get a colour that was somewhat close to their natural tone. This is where I had the idea to create this software that detects, and extracts a person’s most dominant skin colour, then displays it in a colourbar. This information can be used by cosmetic companies, to create a customized foundation/concealer that is suitable for their customers unique tone of skin. 
+        The great thing about this software is that it can be used, not only for cosmetic purposes, but also to customize ‘skin coloured’ tights.""")
         
-         st.write("""The way my software works is, using a method called K-mean clustering. This lets me take an image and group its pixels according to their color. The group with the most (similar) colored pixels will be the most dominant color of your skin. But unfortunately for me it was not that easy, before all of that I had to apply skin detection to make sure the k means didn’t count any background colors (that isn’t skin).
+         st.write("""The way my software works is, using a method called K-mean clustering. This lets me take an image and group its pixels according to their colour. The group with the most (similar) coloured pixels will be the most dominant colour of your skin. But unfortunately for me it was not that easy, before all of that I had to apply skin detection to make sure the k means didn’t count any background colours (that isn’t skin).
         """)
 
 	if choice== 'Detection':
-         st.subheader("Skin and Color Detection")
+         imagg = Image.open('/Users/adeltakawira/Desktop/FSnm.jpg')
+         basewidth = 700
+         wpercent = (basewidth/float(imagg.size[0]))
+         hsize = int((float(imagg.size[1])*float(wpercent)))
+         imagg = imagg.resize((basewidth,hsize), Image.ANTIALIAS)
+         st.image(imagg)
+         st.subheader("Skin and Colour Detection")
          st.write("""  * Take the picture in front of a plain background (ideally a white background)""")
          ("""  * Try to fit only your face in the frame (exclude your clothes, hair and jewelry etc)""")
          ("""  * Use a fairly good quality photo (taken from your phone is fine)""")
@@ -203,18 +218,18 @@ def main():
             st.text("Original Image")
             st.image(image)
             #Converting Uploaded IOByte image into OpenCV Image
-            image1 = cv2.cvtColor(np.array(image),cv2.COLOR_BGR2RGB)
+            image1 = cv2.cvtColour(np.array(image),cv2.COLOR_BGR2RGB)
                        
-         task = ["Skin Color Bar"]
+         task = ["Skin Colour Bar"]
          feature_choice = st.sidebar.selectbox("Choose Features",task)
          if st.button("Process"):
 
-            if feature_choice== "Skin Color Bar":
-                   skin = extractSkin(image1)
-                   dominantColors = extractDominantColor(skin, hasThresholding=True)
-                   color_bar = plotColorBar(dominantColors)
-                   st.text("Color Bar")
-                   st.image(color_bar)
+            if feature_choice== "Skin Colour Bar":
+                   skin = detectSkin(image1)
+                   dominantColours = extractDominantTone(skin, hasThresholding=True)
+                   colour_bar = graphColourBar(dominantColours)
+                   st.text("Colour Bar")
+                   st.image(colour_bar)
                    st.text("Thresholded Image")
                    st.image(cv2.cvtColor(skin, cv2.COLOR_RGB2BGR))
                    image2   = cv2.resize(image1,(500,500))
@@ -226,4 +241,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-    
